bf0c76fb4e1375eb6e6729373ee77aff4f83de3f
yuxin <799746606@qq.com>
2021-06-06 21:47:13 +0800

    #微服务（七）

--- shop-order/src/main/java/com/yuxin/cn/controller/OrderController2.java
+++ shop-order/src/main/java/com/yuxin/cn/controller/OrderController2.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+//@RestController
 @Slf4j
 public class OrderController2 {
 
---
+++ shop-order/src/main/java/com/yuxin/cn/controller/OrderController3.java
@@ -0,0 +1,26 @@
+package com.yuxin.cn.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.yuxin.cn.domain.Order;
+import com.yuxin.cn.domain.Product;
+import com.yuxin.cn.service.OrderService;
+import com.yuxin.cn.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class OrderController3 {
+    @RequestMapping("/order/message1")
+    public String message1(){
+        return "message1";
+    }
+
+    @RequestMapping("/order/message2")
+    public String message2(){
+        return "message2";
+    }
+}

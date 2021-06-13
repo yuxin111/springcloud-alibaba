befe4b20eea22b13ae1f4bb236dee569ebba2fe5
yuxin <799746606@qq.com>
2021-06-13 22:12:27 +0800

    #微服务（十）

---
+++ shop-order/src/main/java/com/yuxin/cn/config/RequestOriginParserDefinition.java
@@ -0,0 +1,26 @@
+package com.yuxin.cn.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.callback.RequestOriginParser;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class RequestOriginParserDefinition implements RequestOriginParser {
+
+    /**
+     * 定义区分来源：本质作用使通过requset来获取来源标识
+     * @param request
+     * @return
+     * app、pc
+     */
+    @Override
+    public String parseOrigin(HttpServletRequest request) {
+        String serviceName = request.getParameter("serviceName");
+        if(StringUtils.isEmpty(serviceName)){
+            throw new RuntimeException("serviceName is empty");
+        }
+        return serviceName;
+    }
+}

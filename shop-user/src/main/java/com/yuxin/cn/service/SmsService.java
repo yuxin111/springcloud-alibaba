889862e8b1b4c1aeca182070b9b0d959d2cb35e8
yuxin <799746606@qq.com>
2021-07-12 22:45:17 +0800

    #微服务（二十四）

--- shop-user/src/main/java/com/yuxin/cn/service/SmsService.java
+++ shop-user/src/main/java/com/yuxin/cn/service/SmsService.java
@@ -3,13 +3,20 @@ package com.yuxin.cn.service;
 import com.alibaba.fastjson.JSONObject;
 import com.yuxin.cn.domain.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.ConsumeMode;
+import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RocketMQMessageListener(consumerGroup = "shop-user",topic = "order-topic")
+@RocketMQMessageListener(
+        consumerGroup = "shop-user",
+        topic = "order-topic",
+        consumeMode = ConsumeMode.CONCURRENTLY, // 消费模式,CONCURRENTLY,ORDERLY
+        messageModel = MessageModel.BROADCASTING// 消息模式,BROADCASTING,CLUSTERING
+)
 public class SmsService implements RocketMQListener<Order> {
     @Override
     public void onMessage(Order order) {

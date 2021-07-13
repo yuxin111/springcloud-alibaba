55641ffe17ee86b7e6a81818aa25d3d03e9196b1
yuxin <799746606@qq.com>
2021-07-13 23:45:47 +0800

    #微服务（二十五）

---
+++ shop-product/src/main/java/com/yuxin/cn/controller/NacosConfigController.java
@@ -0,0 +1,29 @@
+package com.yuxin.cn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class NacosConfigController {
+
+    @Value("${config.appName}")
+    private String appName;
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @RequestMapping("/test-config1")
+    public String testConfig1() {
+        return applicationContext.getEnvironment().getProperty("config.appName");
+    }
+
+    @RequestMapping("/test-config2")
+    public String testConfig2() {
+        return appName;
+    }
+}

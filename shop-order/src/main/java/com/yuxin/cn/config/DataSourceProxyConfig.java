8be041f2f16fa2c7c6eb6ada23ba7276555c5400
yuxin <799746606@qq.com>
2021-08-08 21:24:59 +0800

    #微服务（三十二）

--- shop-order/src/main/java/com/yuxin/cn/config/DataSourceProxyConfig.java
+++ shop-order/src/main/java/com/yuxin/cn/config/DataSourceProxyConfig.java
@@ -1,23 +1,21 @@
 package com.yuxin.cn.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-@Configuration
+//@Configuration
 public class DataSourceProxyConfig {
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DruidDataSource druidDataSource() {
-        return new DruidDataSource();
-    }
-
-    @Primary
-    @Bean
-    public DataSourceProxy dataSource(DruidDataSource druidDataSource) {
-        return new DataSourceProxy(druidDataSource);
-    }
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DruidDataSource druidDataSource() {
+//        return new DruidDataSource();
+//    }
+//
+//    @Primary
+//    @Bean
+//    public DataSourceProxy dataSource(DruidDataSource druidDataSource) {
+//        return new DataSourceProxy(druidDataSource);
+//    }
 }
--- shop-order/src/main/java/com/yuxin/cn/controller/OrderController5.java
+++ shop-order/src/main/java/com/yuxin/cn/controller/OrderController5.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+//@RestController
 @Slf4j
 public class OrderController5 {
 
